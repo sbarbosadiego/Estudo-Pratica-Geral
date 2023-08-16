23e621931e84e94d45bc3d94dab9d9d29c06674a
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-08-15 22:06:14 -0300

    C# Inicio - Módulo 0

---
+++ C#/Estudonauta/Forms/WindowsEx0/WindowsEx0/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsEx0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClique = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClique
+            // 
+            this.btnClique.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClique.Location = new System.Drawing.Point(215, 351);
+            this.btnClique.Name = "btnClique";
+            this.btnClique.Size = new System.Drawing.Size(165, 57);
+            this.btnClique.TabIndex = 0;
+            this.btnClique.Text = "Click";
+            this.btnClique.UseVisualStyleBackColor = true;
+            this.btnClique.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(12, 9);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(776, 86);
+            this.lblMensagem.TabIndex = 1;
+            this.lblMensagem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMensagem.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(407, 353);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(165, 57);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Clear";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnClique);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Janela";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClique;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ C#/Estudonauta/Forms/WindowsEx0/WindowsEx0/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsEx0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            lblMensagem.Text = "Hello, World!";
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lblMensagem.Text = "";
+        }
+    }
+}
