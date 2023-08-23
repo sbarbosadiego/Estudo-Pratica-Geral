f4de5353f0608bacb4d82752ae87d6fe8d60b4ac
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-08-23 19:00:26 -0300

    .

---
+++ C#/Estudonauta/Console/AmbienteModulo01/Modulo01A2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo01A2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(20, 5);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Oi,Tudo Bem?");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
