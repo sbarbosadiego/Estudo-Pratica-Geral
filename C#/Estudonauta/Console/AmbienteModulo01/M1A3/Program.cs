ef7dd36cf8a943159bc79d0137465a9bba3038ff
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-08-25 19:05:56 -0300

    C# - Módulo 1 - A3 a A4

---
+++ C#/Estudonauta/Console/AmbienteModulo01/M1A3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1A3
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Comentário formatado para XML
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Comentário em uma linha
+            Console.WriteLine("Teste do Teste"); // Ainda comentando em linha
+            /*
+             * Comentário em multiplas linhas
+             * Funciona igualmente no java uau
+             */
+            
+            Console.ReadKey();
+        }
+    }
+}
