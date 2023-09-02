d84dcdd5b61953889e649b26a743759604ee91cb
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-09-02 13:56:46 -0300

    .

---
+++ Java/Cod3r/curso-java/exercicios/src/array/Foreach.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		
+		double[] notas = {9.9, 8.5, 7.2, 9.1};
+		
+		
+		for (double nota: notas) {
+			System.out.println(nota);
+		}
+		
+
+	}
+
+}
