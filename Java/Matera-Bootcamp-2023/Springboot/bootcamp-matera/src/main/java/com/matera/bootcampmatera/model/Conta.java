6f8a04cf87069bdf5b5f708ce144a273c7fbdadd
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-09-23 09:53:07 -0300

    Projeto - Dia 5

--- Java/Matera-Bootcamp-2023/Springboot/bootcamp-matera/src/main/java/com/matera/bootcampmatera/model/Conta.java
+++ Java/Matera-Bootcamp-2023/Springboot/bootcamp-matera/src/main/java/com/matera/bootcampmatera/model/Conta.java
@@ -5,59 +5,46 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Builder;
+import jakarta.persistence.OneToOne;
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
-@Builder
-@Entity // CONTA
-@Table(name = "Nome_da_minha_tabela")
+@Entity
 public class Conta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "numero")
-    private String numeroConta; //NUMERO_CONTA
-    private String agencia; // AGENCIA
-
+    @Column(name = "NUMERO")
+    private String numeroConta;
+    @Column(name = "AGENCIA")
+    private String agencia;
+    @Column(name = "SALDO")
+    private BigDecimal saldo = BigDecimal.ZERO;
+    @OneToOne
+    @Column(name = "TITULAR")
+    private Titular titular;
 
     public Conta() {
     }
 
-    public Conta(Long id, String numeroConta, String agencia) {
+    public Conta(Long id, String numeroConta, String agencia, Titular titular, BigDecimal saldo) {
         this.id = id;
         this.numeroConta = numeroConta;
         this.agencia = agencia;
+        this.titular = titular;
+        this.saldo = BigDecimal.ZERO;
     }
 
-	public Long getId() {
-		return id;
-	}
+    public void debito(BigDecimal valor){
+        saldo = saldo.subtract(valor);
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void credito(BigDecimal valor){
+        saldo = saldo.add(valor);
+    }
 
-	public String getNumeroConta() {
-		return numeroConta;
-	}
-
-	public void setNumeroConta(String numeroConta) {
-		this.numeroConta = numeroConta;
-	}
-
-	public String getAgencia() {
-		return agencia;
-	}
-
-	public void setAgencia(String agencia) {
-		this.agencia = agencia;
-	}
-    
-    
 }
 
 /**
