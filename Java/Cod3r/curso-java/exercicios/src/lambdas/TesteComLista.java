6c0d3005c8dd081550661232b8e80933164645f3
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-09-29 19:44:30 -0300

    .

--- Java/Cod3r/curso-java/exercicios/src/lambdas/TesteComLista.java
+++ Java/Cod3r/curso-java/exercicios/src/lambdas/TesteComLista.java
@@ -11,7 +11,10 @@ public class TesteComLista {
 		
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		
-		// Em filter é passado o parâmetro na lambda para filtrar valores em que a divisão por dois seja de resto igual a 0
+		/**
+		 * Em filter é passado o parâmetro na lambda para filtrar valores 
+		 * em que a divisão por dois seja de resto igual a 0
+		 */
 		List<Integer> numerosPares = numeros.stream()
 		                                    .filter(n -> n % 2 == 0)
 		                                    .collect(Collectors.toList());
