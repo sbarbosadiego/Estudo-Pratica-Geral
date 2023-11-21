fe0431b8807a59ed287171f28424e53c220b6274
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-11-21 19:38:42 -0300

    C# - M1A5

---
+++ C#/Estudonauta/Console/AmbienteModulo01/M01A05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ao utilizar var é automaticamente definido um tipo para a variável
+            var numeroVar = 0;
+            byte numeroByte = 255;
+
+            Console.WriteLine("O valor da variável numero é: " + numeroVar + " e seu tipo é " + numeroVar.GetType());
+            Console.WriteLine("O valor da variável numero é: " + numeroByte + " e seu tipo é " + numeroByte.GetType());
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ C#/Estudonauta/Console/AmbienteModulo01/M1A5b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1A5b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte idade = 25;
+            short publico = 32_423;
+            float media = 4.5f;
+            decimal grande = 235346234M;
+
+
+
+        }
+    }
+}
