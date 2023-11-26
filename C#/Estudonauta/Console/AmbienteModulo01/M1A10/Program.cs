e073e6e0f7f4000a691a334422a7527c3102cb0d
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-11-26 11:56:37 -0300

    C# - Aula 10

---
+++ C#/Estudonauta/Console/AmbienteModulo01/M1A10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1A10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dia = DateTime.Now.Day;
+            int mes = DateTime.Now.Month;
+            int ano = DateTime.Now.Year;
+            int hora = DateTime.Now.Hour;
+            int minutos = DateTime.Now.Minute;
+
+            Console.WriteLine($"Data atual: {dia}/{mes}/{ano}");
+            Console.WriteLine($"Hora atual: {hora}:{minutos}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/Estudonauta/Console/AmbienteModulo01/M1A9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1A9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random gerador = new Random();
+            int valorGerado = gerador.Next(100);
+            Console.WriteLine($"Valor garado: {valorGerado}");
+            Console.ReadKey();
+
+        }
+    }
+}
