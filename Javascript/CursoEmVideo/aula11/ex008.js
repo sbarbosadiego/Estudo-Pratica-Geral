d72f5920db4f3c097a080bd55e6bab0bc342ba75
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-11-29 08:10:56 -0300

    Javascript - Aula 11

---
+++ Javascript/CursoEmVideo/aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 60.9
+
+if (velocidade > 60.0) {
+    console.log(`Foi ultrapassado o limite de velocidade de 60km/h \nVelocidade atual: ${velocidade}`)
+} else {
+    console.log(`Você está dentro do limite de velocidade \nVelocidade atual: ${velocidade}`)
+}
---
+++ Javascript/CursoEmVideo/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+
+if (pais == 'Brasil') {
+    console.log(`Parabéns você nasceu no inferno`)
+} else {
+    console.log(`Parabéns você não nasceu no inferno`)
+}
