14db5067e6872fa50cb9e6490100648d1ad5330d
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-12-07 21:07:43 -0300

    C# - Aula 12

---
+++ C#/Estudonauta/Console/AmbienteModulo01/M1A11a/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace M1A11a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, ano;
+
+            dia = DateTime.Now.Day;
+            mes = DateTime.Now.Month;
+            ano = DateTime.Now.Year;
+            Console.WriteLine($"Hoje é dia {dia}");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Mês {mes}");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Do ano de {ano}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/Estudonauta/Console/AmbienteModulo01/M1A11b/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace M1A11b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(142, 141);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(77, 30);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(38, 41);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 25);
+            this.lblMensagem.TabIndex = 1;
+            this.lblMensagem.Click += new System.EventHandler(this.lblMensagem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 236);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblMensagem;
+    }
+}
+
