3585cde46dd208cdca780d76cf314d33d91649b8
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-12-23 14:35:53 -0300

    C# - Aula 7

---
+++ C#/Estudonauta/AmbienteM2/M2A7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2A7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valor = { 0, 0};
+
+            Console.Write("Digite o primeiro número: ");
+            int.TryParse(Console.ReadLine(), out valor[0]);
+            Console.Write("Digite o segundo número: ");
+            int.TryParse(Console.ReadLine(), out valor[1]);
+
+            if (valor[0] == valor[1])
+            {
+                Console.WriteLine($"O número {valor[0]} é igual ao número {valor[1]}");
+            }
+            else if (valor[0] > valor[1] || valor[1] > valor[0])
+            {
+                if (valor[0] > valor[1])
+                {
+                    Console.WriteLine($"O número {valor[0]} é maior que o número {valor[1]}");
+                } else
+                {
+                    Console.WriteLine($"O número {valor[1]} é maior que o número {valor[0]}");
+                }
+            } 
+
+            Console.ReadKey();
+        }
+    }
+}
