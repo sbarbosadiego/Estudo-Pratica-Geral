6eda55941ad62ddfc1f08eefb4dc6392a310bfff
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-12-26 08:13:33 -0300

    C# - M2A11

---
+++ C#/Estudonauta/AmbienteM2/M2A10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2A10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeAluno;
+            float[] nota = {0, 0, 0};
+            float media = 0;
+
+            Console.Write("Digite seu nome: ");
+            nomeAluno = Console.ReadLine();
+
+            for (int c = 0; c < nota.Length; c++)
+            {
+                Console.Write($"Digite a {c + 1}ª nota: ");
+                float.TryParse(Console.ReadLine(), out nota[c]);
+                media += nota[c];
+            }
+
+            media = media / nota.Length;
+
+            if (media >= 0 && media < 4)
+            {
+                Console.WriteLine($"A média foi: {media:F2}\nO aluno {nomeAluno} está REPROVADO");
+            } else if (media >= 4 && media < 7)
+            {
+                Console.WriteLine($"A média foi: {media:F2}\nO aluno {nomeAluno} está de RECUPERAÇÃO");
+            } else if (media >= 7 && media <= 10)
+            {
+                Console.WriteLine($"A média foi: {media:F2}\nO aluno {nomeAluno} está APROVADO");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/Estudonauta/AmbienteM2/M2A9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2A9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valorUm = true;
+            bool valorDois = false;
+
+            Console.WriteLine($"Sendo p = {valorUm} e q = {valorDois}");
+            Console.WriteLine($"NOT p = {!valorUm} \nNOT q = {!valorDois}");
+            Console.WriteLine($"{valorUm} AND {valorDois} = {valorUm && valorDois}");
+            Console.WriteLine($"{valorUm} OR {valorDois} = {valorUm ||  valorDois}");
+            Console.ReadKey();
+
+        }
+    }
+}
