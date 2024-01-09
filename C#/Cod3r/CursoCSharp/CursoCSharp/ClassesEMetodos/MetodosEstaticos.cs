aa5a74f8f29d399757b29b301fa6b9bce76f2c1f
Diego Barbosa <diego.barbosa.pro@gmail.com>
2024-01-08 21:12:51 -0300

    Sobe

---
+++ C#/Cod3r/CursoCSharp/CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class CalculadoraEstatica
+    {
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    internal class MetodosEstaticos
+    {
+        public static void Executar()
+        {
+            var resultado = CalculadoraEstatica.Somar(5, 5);
+            var resultadoDois = new CalculadoraEstatica();
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultadoDois.Multiplicar(2, 3));
+        }
+    }
+}
--- C#/Cod3r/CursoCSharp/CursoCSharp/Program.cs
+++ C#/Cod3r/CursoCSharp/CursoCSharp/Program.cs
@@ -37,7 +37,8 @@ namespace CursoCSharp
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
                 {"Construtores - Classes e Métodos", Construtores.Executar},
-                {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar}
+                {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar},
+                {"Métodos Estáticos - Classes e Métodos", MetodosEstaticos.Executar}
             });
 
             central.SelecionarEExecutar();
