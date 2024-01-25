f8e5eb742f29ffb6f866e77b88ff71654a5d93e7
Diego Barbosa <diego.barbosa.pro@gmail.com>
2024-01-25 20:25:09 -0300

    C# - Cod3r - Aula 82/109

---
+++ C#/Cod3r/CursoCSharp/CursoCSharp/OO/ConstrutorThis.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO
+{
+    internal class ConstrutorThis
+    {
+        public class Animal
+        {
+            public string Nome { get; set; }
+
+            public Animal(string nome)
+            {
+                Nome = nome;
+            }
+        }
+
+        public class Cachorro : Animal
+        {
+            public double Altura { get; set; }
+
+            public Cachorro(string nome) : base (nome)
+            {
+                Console.WriteLine($"Cachorro {nome}");
+            }
+
+            public Cachorro(string nome, double altura) : this(nome)
+            {
+                Altura = altura;
+            }
+
+            public override string ToString()
+            {
+                return $"{Nome} tem {Altura}cm de altura";
+            }
+        }
+
+        public static void Executar()
+        {
+            var cachorroUm = new Cachorro("Spike");
+            var cachorroDois = new Cachorro("Max", 40.0);
+
+            Console.WriteLine(cachorroUm.ToString());
+            Console.WriteLine(cachorroDois.ToString());
+        }
+    }
+}
