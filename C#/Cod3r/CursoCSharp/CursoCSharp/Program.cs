22a11b61bd8c1de9586b929df3ec6cf9a3431a30
Diego Barbosa <diego.barbosa.pro@gmail.com>
2024-01-26 18:52:12 -0300

    Sobe

--- C#/Cod3r/CursoCSharp/CursoCSharp/Program.cs
+++ C#/Cod3r/CursoCSharp/CursoCSharp/Program.cs
@@ -66,6 +66,8 @@ namespace CursoCSharp
 
                 // Orientação a Objetos
                 {"Herança - Orientação a Objetos", Heranca.Executar},
+                {"Construtor This - Orientação a Objetos", ConstrutorThis.Executar},
+                {"Encapsulamento - Orientação a Objetos", Encapsulamento.Executar},
 
 
 
---
+++ C#/Cod3r/CursoCSharp/Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class SubCelebridade
+    {
+        // Todos
+        public string InfoPublica = "Tenho um instagram";
+
+        // Herança
+        protected string CorDoOlho = "Verde";
+
+        // Mesmo projeto (Assembly)
+        internal ulong NumeroCelular = 55119999999;
+
+        // Herança ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso muitas gírias";
+
+        // Mesma classe ou herança no mesmo projeto (C# > 7.2)
+        private protected string SegredoFamilia = "Ugaaa";
+
+        // Private é o padrão
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("Subcelebridadre: ");
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
