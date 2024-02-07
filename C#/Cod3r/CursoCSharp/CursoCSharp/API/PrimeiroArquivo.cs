b6a8cc43952b9ae6490161ed45b2f3af459f0b47
Diego Barbosa <diego.barbosa.pro@gmail.com>
2024-02-07 19:45:47 -0300

    C# - Cod3r - Aula 96/109

--- C#/Cod3r/CursoCSharp/CursoCSharp/API/PrimeiroArquivo.cs
+++ C#/Cod3r/CursoCSharp/CursoCSharp/API/PrimeiroArquivo.cs
@@ -2,17 +2,30 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace CursoCSharp.API
 {
+    public static class EstensaoString
+    {
+        public static string ParseHome(this string path)
+        {
+            string home = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                Environment.OSVersion.Platform == PlatformID.MacOSX)
+                ? Environment.GetEnvironmentVariable("HOME")
+                : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            return path.Replace("~", home);
+        }
+    }
+
     internal class PrimeiroArquivo
     {
         public static void Executar()
         {
             // @ antes da string interpreta de forma literal
-            var path = @"~/primeiro_arquivo.txt";
+            var path = @"~/primeiro_arquivo.txt".ParseHome();
 
-            if (File.Exists(path))
+            if (!File.Exists(path))
             {
                 using (StreamWriter sw = File.CreateText(path))
                 {
@@ -20,6 +33,12 @@ namespace CursoCSharp.API
                     sw.WriteLine("eh mole eh mole eh mole");
                     sw.WriteLine("eh mole eh mole eh mole");
                 }
+            }
+
+            using (StreamWriter sw = File.AppendText(path)) {
+                sw.WriteLine("eh duro");
+                sw.WriteLine("eh duro");
+                sw.WriteLine("");
             }
         }
     }
